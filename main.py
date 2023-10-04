8703398cb90786962e6f931c0170c188c48ec279
alchemzy <ryueda@protonmail.com>
2023-10-04 16:23:09 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+import json
+
+REVIEWS_FILE = 'reviews.txt'
+DATA_FILE = 'data.json'
+
+def compute_sentiments(file):
+    reviews = None
+    with open(file) as file:
+        reviews = [line.strip() for line in file.readlines()]
+    if reviews:
+        sentiment_pipeline = pipeline("text-classification", model="LiYuan/amazon-review-sentiment-analysis")
+        sentiments = sentiment_pipeline(reviews)
+        data = []
+
+        i = 0
+        for review, sentiment in zip(reviews, sentiments):
+            data.append({
+                'id': i,
+                'review': review,
+                'star rating': sentiment['label'],
+                'score': sentiment['score']
+            })
+            i += 1
+        with open(DATA_FILE, 'w') as data_file:
+            data_file.write(json.dumps(data))
+
+
+if __name__ == '__main__':
+    compute_sentiments(REVIEWS_FILE)
